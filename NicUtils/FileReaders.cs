5cb5f33a1d91a5054273e8250d9b58270b041f6f
Nicholas Holgate <niceholgate@gmail.com>
2023-08-28 23:54:51 +0800

    Improve CSVReader - Read once then can GetData() in different types. Also adds optional headers.

--- NicUtils/FileReaders.cs
+++ NicUtils/FileReaders.cs
@@ -5,14 +5,32 @@ using System.Linq;
 using System.Collections;
 
 namespace NicUtils {
-    // TODO: null for empty cell
-    // TODO: support column headers
-    public static class CSVReader {
-        public static List<List<string>> ReadCSV(string filepath, char splitChar = ',') {
+
+    public class CSVReader {
+
+        public bool WithHeaders {  get; private set; }
+
+        public char SplitChar { get; private set; }
+
+        public List<string> Headers { get; private set; } = null;
+
+        private List<List<string>> stringData = new();
+
+        public CSVReader(string filepath, bool withHeaders, char splitChar = ',') {
+            WithHeaders = withHeaders;
+            SplitChar = splitChar;
+            ReadCSV(filepath);
+        }
+
+        private void ReadCSV(string filepath) {
             StreamReader reader = new StreamReader(filepath);
-            List<List<string>> table = new();
             while (!reader.EndOfStream) {
-                table.Add(reader.ReadLine().Split(splitChar)
+                string line = reader.ReadLine();
+                if (WithHeaders && Headers == null) {
+                    Headers = line.Split(SplitChar).Select(el => el.Trim()).ToList();
+                    continue;
+                }
+                stringData.Add(line.Split(SplitChar)
                     .Select(el => {
                         string trimmed = el.Trim();
                         if (trimmed.Length == 0) return null;
@@ -21,7 +39,14 @@ namespace NicUtils {
                     .ToList());
             }
             reader.Close();
-            return table;
+        }
+
+        public List<List<T>> GetData<T>() {
+            List<List<T>> dataConverted = new();
+            foreach (List<string> row in stringData) {
+                dataConverted.Add(row.Select(el => el.AttemptConversion<T>().result).ToList()) ;
+            }
+            return dataConverted;
         }
     }
 }
--- NicUtilsTest/FileReadersTests.cs
+++ NicUtilsTest/FileReadersTests.cs
@@ -6,12 +6,57 @@ using NicUtils;
 [TestClass]
 public class FileReadersTests {
     [TestMethod]
-    public void ReadFloatDataWithoutHeaders() {
-        List<List<string>> data = CSVReader.ReadCSV("../../../Resources/floatDataWithoutHeaders.csv");
+    public void ReadNullableFloatDataWithoutHeaders() {
+        CSVReader reader = new("../../../Resources/floatDataWithoutHeaders.csv", false);
+        List<List<double?>> data = reader.GetData<double?>();
+        List<string> headers = reader.Headers;
+        
         int expectedRows = 3;
         Assert.AreEqual(expectedRows, data.Count);
-        AssertSequencesAreEqual(data.ElementAt(0), new List<string>(){ "3.45", "6.4", "4.63" });
-        AssertSequencesAreEqual(data.ElementAt(1), new List<string>() { "356.66", "33", "486.653" });
-        AssertSequencesAreEqual(data.ElementAt(2), new List<string>() { null, "0", "1" });
+        Assert.IsNull(headers);
+        AssertSequencesAreEqual(data.ElementAt(0), new List<double?>() { 3.45, 6.4, 4.63 });
+        AssertSequencesAreEqual(data.ElementAt(1), new List<double?>() { 356.66, 33, 486.653 });
+        AssertSequencesAreEqual(data.ElementAt(2), new List<double?>() { null, 0, 1 });
+    }
+
+    [TestMethod]
+    public void ReadNotNullableFloatDataWithoutHeaders() {
+        CSVReader reader = new("../../../Resources/floatDataWithoutHeaders.csv", false);
+        List<List<double>> data = reader.GetData<double>();
+        List<string> headers = reader.Headers;
+
+        int expectedRows = 3;
+        Assert.AreEqual(expectedRows, data.Count);
+        Assert.IsNull(headers);
+        AssertSequencesAreEqual(data.ElementAt(0), new List<double>() { 3.45, 6.4, 4.63 });
+        AssertSequencesAreEqual(data.ElementAt(1), new List<double>() { 356.66, 33, 486.653 });
+        AssertSequencesAreEqual(data.ElementAt(2), new List<double>() { 0, 0, 1 });
+    }
+
+    [TestMethod]
+    public void ReadNullableFloatDataWithHeaders() {
+        CSVReader reader = new("../../../Resources/floatDataWithHeaders.csv", true);
+        List<List<double?>> data = reader.GetData<double?>();
+        List<string> headers = reader.Headers;
+
+        int expectedRows = 3;
+        Assert.AreEqual(expectedRows, data.Count);
+        AssertSequencesAreEqual(headers, new List<string>() { "col1", "col2", "col3" });
+        AssertSequencesAreEqual(data.ElementAt(0), new List<double?>() { 3.45, 6.4, 4.63 });
+        AssertSequencesAreEqual(data.ElementAt(1), new List<double?>() { 356.66, 33, 486.653 });
+        AssertSequencesAreEqual(data.ElementAt(2), new List<double?>() { null, 0, 1 });
+    }
+
+    [TestMethod]
+    public void ReadTextDataWithHeaders() {
+        CSVReader reader = new("../../../Resources/textDataWithHeaders.csv", true);
+        List<List<string>> data = reader.GetData<string>();
+        List<string> headers = reader.Headers;
+
+        int expectedRows = 2;
+        Assert.AreEqual(expectedRows, data.Count);
+        AssertSequencesAreEqual(headers, new List<string>() { "col1", "col2", "col3" });
+        AssertSequencesAreEqual(data.ElementAt(0), new List<string>() { "hello", null, "col 2 is empty!" });
+        AssertSequencesAreEqual(data.ElementAt(1), new List<string>() { "goodbye", "this col 2 is not empty", "2.678" });
     }
 }
